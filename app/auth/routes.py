9b790e89eb0c2855200e9a4509f2a7da66a158a8
ethannakamura <93556831+ethannakamura@users.noreply.github.com>
2022-01-08 15:15:20 -0600

    second updates

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, render_template
 auth = Blueprint('auth',__name__, template_folder='auth_templates')
 
-@auth.route('/login')
+@auth.route('/Login')
 def login():
     return render_template('login.html')
 
-@auth.route('/logout')
+@auth.route('/LogoutSuccessful')
 def logout():
     return render_template('logout.html')
 
-@auth.route('/signup')
+@auth.route('/Signup')
 def sign_up():
     return render_template('signup.html')
--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,22 @@ from flask import Blueprint
 def home():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/MyDashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/MyRentalBoard')
+def rentalboard():
+    return render_template('rentalboard.html')
+
+@app.route('/RocketCRM')
+def rocketcrm():
+    return render_template('rocketcrm.html')
+
+@app.route('/MyWholesaling')
+def wholesaling():
+    return render_template('wholesaling.html')
+
+@app.route('/MyHouseFlipping')
+def flipping():
+    return render_template('flipping.html')
