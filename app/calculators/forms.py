8cca5611461b581ae7549396bc0cc564be30c3d8
ethannakamura <93556831+ethannakamura@users.noreply.github.com>
2022-01-18 16:26:19 -0600

    commented out calculator syntax for editing

--- app/calculators/forms.py
+++ app/calculators/forms.py
@@ -3,6 +3,21 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+class rentalincomeForm(FlaskForm):
+    rentalincome = StringField('rentalincome', validators=[DataRequired()])
+    miscsvcincome = StringField('miscsvcincome', validators=[DataRequired()])
+    submit = SubmitField()
+
+class debtlincomeForm(FlaskForm):
+    rentalincome = StringField('rentalincome', validators=[DataRequired()])
+    miscsvcincome = StringField('miscsvcincome', validators=[DataRequired()])
+    submit = SubmitField()
+
+class profitincomeForm(FlaskForm):
+    rentalincome = StringField('rentalincome', validators=[DataRequired()])
+    miscsvcincome = StringField('miscsvcincome', validators=[DataRequired()])
+    submit = SubmitField()
+
 #class rentalincomeForm(FlaskForm):
     #rentalincome = StringField('rentalincome', validators=[DataRequired()])
     #miscsvcincome = StringField('miscsvcincome', validators=[DataRequired()])
--- app/calculators/routes.py
+++ app/calculators/routes.py
@@ -6,6 +6,36 @@ from flask import render_template
 
 calculators = Blueprint('calculators',__name__, template_folder='templates')
 
+@calculators.route('/MyRentalBoard', methods=['GET', 'POST'])
+def rincome():
+    form = rentalincomeForm()
+    morentalincome = form.rentalincome.data
+    momiscsvcincome = form.miscsvcincome.data
+    form2 = debtlincomeForm()
+    morentalincome = form2.rentalincome.data
+    momiscsvcincome = form2.miscsvcincome.data
+    form3 = profitincomeForm()
+    morentalincome = form3.rentalincome.data
+    momiscsvcincome = form3.miscsvcincome.data
+    tmi2 = []
+    tmi3 = []
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            tmi = float(morentalincome) + float(momiscsvcincome)
+            tmi2.append(tmi)
+            print('info received')  
+        elif form2.validate_on_submit():
+            tmi = float(morentalincome) + float(momiscsvcincome)
+            tmi3.append(tmi)
+            print('info received2')
+        elif form3.validate_on_submit():
+            tmi = morentalincome + momiscsvcincome
+            print('info received3')
+        else:
+            print('info not received')
+
+    return render_template('rentalboard.html', form=form, form2=form2, tmi2=tmi2, tmi3=tmi3)
+
 #@calculators.route('/MyRentalBoard', methods=['GET', 'POST'])
 #def rincome():
     #form = rentalincomeForm()
