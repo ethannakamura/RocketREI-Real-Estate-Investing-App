ddafaa06a31e15dcbc0dd2cd978ee35271811038
ethannakamura <93556831+ethannakamura@users.noreply.github.com>
2022-01-19 14:53:59 -0600

    arrow update

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config:
     FLASK_ENV =os.environ.get('FLASK_ENV')
     SECRET_KEY =os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI =os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS =False 
+    SQLALCHEMY_TRACK_MODIFICATIONS =False
