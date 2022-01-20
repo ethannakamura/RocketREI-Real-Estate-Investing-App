4142b95a123274676ef7a1de3cffc66fd91dd0ee
ethannakamura <93556831+ethannakamura@users.noreply.github.com>
2022-01-19 21:34:41 -0600

    2 pages designed

--- app/calculators/forms.py
+++ app/calculators/forms.py
@@ -5,10 +5,11 @@ from wtforms.validators import DataRequired
 
 #below code was used for Coding Options #2 and #3 in creating calculator forms in the rentalboard route:
 
-class rentalinvestingForm(FlaskForm):
+class initialincomeandexpensesForm(FlaskForm):
     # - addition - calculate total monthly income
     rentalincome = StringField('rentalincome', validators=[DataRequired()])
     miscsvcincome = StringField('miscsvcincome', validators=[DataRequired()])
+    submit = SubmitField()
 
     # - addition - calculate total monthly expenses 
     proptax = StringField('proptax', validators=[DataRequired()])
@@ -16,7 +17,6 @@ class rentalinvestingForm(FlaskForm):
     watersewer = StringField('watersewer', validators=[DataRequired()])
     garbage = StringField('garbage', validators=[DataRequired()])
     electricity = StringField('electric', validators=[DataRequired()])
-    
     gas = StringField('gas', validators=[DataRequired()])
     hoafees = StringField('hoafees', validators=[DataRequired()])
     lawn = StringField('lawn', validators=[DataRequired()])
@@ -25,20 +25,28 @@ class rentalinvestingForm(FlaskForm):
     capex = StringField('capex', validators=[DataRequired()])
     propmgmt = StringField('propmgmt', validators=[DataRequired()])
     mortgage = StringField('mortgage', validators=[DataRequired()])
+    submit = SubmitField()
 
+class totalincomeandexpensesForm(FlaskForm):
     # - substraction - calculate total monthly cash flow (total income - total expenses)
     totalincome =  StringField('totalincome', validators=[DataRequired()])
     totalexpenses =  StringField('totalincome', validators=[DataRequired()])
+    submit = SubmitField()
 
+class totalmonthlycashflowForm(FlaskForm):
     # - multiplication - calculate total monthly income (totalmonthlycashflow * 12)
     totalmonthlycashflow =  StringField('totalincome', validators=[DataRequired()])
+    submit = SubmitField()
 
+class requiredinvestmentForm(FlaskForm):
     # - addition - calculate total investment (total investment = dp + cc + rb + cf)
     downpayment = StringField('proptax', validators=[DataRequired()])
     closingcosts = StringField('proptax', validators=[DataRequired()])
     rehabbudget = StringField('proptax', validators=[DataRequired()])
     contingencyfund = StringField('proptax', validators=[DataRequired()])
+    submit = SubmitField()
 
+class cashoncashreturnForm(FlaskForm):
     # - division - calculate the cash on cash return (annual cash flow divided(/) by total invesment multiplied(*) by 100)
     annualcashflow = StringField('proptax', validators=[DataRequired()])
     totalinvestment = StringField('proptax', validators=[DataRequired()])
--- app/calculators/routes.py
+++ app/calculators/routes.py
@@ -1,65 +1,132 @@
 from flask import Blueprint, render_template, request
 
-from app.calculators.forms import rentalinvestingForm
+from app.calculators.forms import initialincomeandexpensesForm, totalincomeandexpensesForm, totalmonthlycashflowForm, requiredinvestmentForm, cashoncashreturnForm
 
 from flask import render_template
 
 calculators = Blueprint('calculators',__name__, template_folder='templates')
 
 @calculators.route('/InitialPropertyIncomeExpenses', methods=['GET', 'POST'])
-def rincome():
-    form = rentalinvestingForm()
-    morentalincome = form.rentalincome.data
-    momiscsvcincome = form.miscsvcincome.data
-    moproptax = form.proptax.data
-    moinsurance = form.insurance.data
-    mowatersewer = form.watersewer.data
-    mogarbage = form.garbage.data
-    moelectricity = form.electricity.data
-    mogas = form.gas.data
-    mohoafees = form.hoafees.data
-    molawn = form.lawn.data
-    movacancy = form.vacancy.data
-    morepairs = form.repairs.data
-    mocapex = form.capex.data
-    mopropmgmt = form.propmgmt.data
-    momortgage = form.mortgage.data
-    mototalincome = form.totalincome.data
-    mototalexpenses = form.totalexpenses.data
-    annualcashflow = form.totalmonthlycashflow.data
-    onlydownpayment = form.downpayment.data
-    onlyclosingcosts = form.closingcosts.data
-    onlyrehabbudget = form.rehabbudget.data
-    onlycontingencyfund = form.contingencyfund.data
-    myannualcashflow = form.annualcashflow.data
-    mytotalinvestment = form.totalinvestment.data
+def incexp():
+    form1 = initialincomeandexpensesForm()
+    morentalincome = form1.rentalincome.data
+    momiscsvcincome = form1.miscsvcincome.data
+    moproptax = form1.proptax.data
+    moinsurance = form1.insurance.data
+    mowatersewer = form1.watersewer.data
+    mogarbage = form1.garbage.data
+    moelectricity = form1.electricity.data
+    mogas = form1.gas.data
+    mohoafees = form1.hoafees.data
+    molawn = form1.lawn.data
+    movacancy = form1.vacancy.data
+    morepairs = form1.repairs.data
+    mocapex = form1.capex.data
+    mopropmgmt = form1.propmgmt.data
+    momortgage = form1.mortgage.data
 
     calc1 = []
+
     if request.method == 'POST':
-        if form.validate_on_submit():
+        if form1.validate_on_submit():
             res1 = float(morentalincome) + float(momiscsvcincome)
             resonepointfive = int(res1)
+
             res2 = float(moproptax) + float(moinsurance) + float(mowatersewer) + float(mogarbage) + float(moelectricity) + float(mogas) + float(mohoafees) + float(molawn) + float(movacancy) + float(morepairs) + float(mocapex) + float(mopropmgmt) + float(momortgage)
             restwopointfive = int(res2)
-            res3 = float(mototalincome) - float(mototalexpenses)
-            resthreepointfive = int(res3)
-            res4 = float(annualcashflow)
-            resfourpointfive = int(res4) * int(12)
-            res5 = float(onlydownpayment) + float(onlyclosingcosts) + float(onlyrehabbudget) + float(onlycontingencyfund)
-            resfivepointfive = int(res5)
-            res6 = float(myannualcashflow) / float(mytotalinvestment)
-            ressixpointfive = float(res6) * float(100)
+
             calc1.append(resonepointfive)
             calc1.append(restwopointfive)
-            calc1.append(resthreepointfive)
-            calc1.append(resfourpointfive)
-            calc1.append(resfivepointfive)
-            calc1.append(ressixpointfive)
+
             print('info received')
         else:
             print('info not received')
 
-    return render_template('incexp.html', form=form, calc1=calc1)
+    return render_template('incexp.html', form1=form1, calc1=calc1)
+
+@calculators.route('/MonthlyIncomeExpenses', methods=['GET', 'POST'])
+def moincexp():
+    form2 = totalincomeandexpensesForm()
+    mototalincome = form2.totalincome.data
+    mototalexpenses = form2.totalexpenses.data
+
+    calc2 = []
+
+    if request.method == 'POST':
+        if form2.validate_on_submit():
+            res3 = float(mototalincome) - float(mototalexpenses)
+            resthreepointfive = int(res3)
+            calc2.append(resthreepointfive)
+            print('info received')
+
+        else:
+            print('info not received')
+
+    return render_template('moincexp.html', form2=form2, calc2=calc2)
+
+
+@calculators.route('/MonthlyCashflow', methods=['GET', 'POST'])
+def mocashflow():   
+    form3 = totalmonthlycashflowForm()
+    annualcashflow = form3.totalmonthlycashflow.data
+
+    calc3 = []
+
+    if request.method == 'POST':
+        if form3.validate_on_submit():
+            res4 = float(annualcashflow)
+            resfourpointfive = int(res4) * int(12)
+            calc3.append(resfourpointfive)
+
+            print('info received')
+        else:
+            print('info not received')
+
+    return render_template('moincexp.html', form3=form3, calc3=calc3)
+
+@calculators.route('/InvestmentAmount', methods=['GET', 'POST'])
+def requiredinvestment():
+    form4 = requiredinvestmentForm()
+    onlydownpayment = form4.downpayment.data
+    onlyclosingcosts = form4.closingcosts.data
+    onlyrehabbudget = form4.rehabbudget.data
+    onlycontingencyfund = form4.contingencyfund.data
+
+    calc4 = []
+
+    if request.method == 'POST':
+        if form4.validate_on_submit():
+            res5 = float(onlydownpayment) + float(onlyclosingcosts) + float(onlyrehabbudget) + float(onlycontingencyfund)
+            resfivepointfive = int(res5)
+
+            calc4.append(resfivepointfive)
+
+            print('info received')
+        else:
+            print('info not received')
+
+    return render_template('moincexp.html', form4=form4, calc4=calc4)
+
+@calculators.route('/CashonCashReturn', methods=['GET', 'POST'])
+def cashoncash():
+    form5 = cashoncashreturnForm()
+    myannualcashflow = form.annualcashflow.data
+    mytotalinvestment = form.totalinvestment.data
+
+    calc5 = []
+
+    if request.method == 'POST':
+        if form5.validate_on_submit():
+
+            res6 = float(myannualcashflow) / float(mytotalinvestment)
+            ressixpointfive = float(res6) * float(100)
+            calc5.append(ressixpointfive)
+
+            print('info received')
+        else:
+            print('info not received')
+
+    return render_template('moincexp.html', form5=form5, calc5=calc5)
 
 # Coding Option #2 
 #----submit form rendering all form results bus only as separate list values [111,222.333]
